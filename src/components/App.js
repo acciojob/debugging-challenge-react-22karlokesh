94b45e825c847b0d2bed9c130e7931b005ede0cb
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2026-09-14 21:03:44 +0530

    Fix formatting and event handling in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import '../styles/App.css';
 
 const App = () => {
   let [count, setCount] = useState(0)
-
-  
   return (
     <div class="ball">
           <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
